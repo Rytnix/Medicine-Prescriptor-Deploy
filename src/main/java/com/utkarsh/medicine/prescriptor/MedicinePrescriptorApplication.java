8bb47f2a7f4cc823a81b560fb0adf32272a72015
Utkarsh Yadav <utkarshrock1510@gmail.com>
2022-09-04 10:49:17 +0530

    reconfigration

--- src/main/java/com/utkarsh/medicine/prescriptor/MedicinePrescriptorApplication.java
+++ src/main/java/com/utkarsh/medicine/prescriptor/MedicinePrescriptorApplication.java
@@ -5,9 +5,31 @@ import com.utkarsh.medicine.prescriptor.Service.MedicineService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.HttpEncodingAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Configuration
+@Import({
+		DispatcherServletAutoConfiguration.class,
+		ErrorMvcAutoConfiguration.class,
+		HttpEncodingAutoConfiguration.class,
+		HttpMessageConvertersAutoConfiguration.class,
+		JacksonAutoConfiguration.class,
+		PropertyPlaceholderAutoConfiguration.class,
+		ThymeleafAutoConfiguration.class,
+		WebMvcAutoConfiguration.class,
+
+})
 public class MedicinePrescriptorApplication {
 
 	public static void main(String[] args) {
