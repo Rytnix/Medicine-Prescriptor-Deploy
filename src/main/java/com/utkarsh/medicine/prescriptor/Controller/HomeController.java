dafe8b5d4509d7448e21ac2d818f3f79c5e0cf4a
Utkarsh Yadav <utkarshrock1510@gmail.com>
2023-02-13 01:23:08 +0530

    fixes

--- src/main/java/com/utkarsh/medicine/prescriptor/Controller/HomeController.java
+++ src/main/java/com/utkarsh/medicine/prescriptor/Controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.utkarsh.medicine.prescriptor.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/MedicinePrescriptor")
+@CrossOrigin(origins = "*")
 public class HomeController {
 
 
